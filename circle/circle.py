607bb7f0bcdcb6184fc27d2962212f994b0d8881
analamour <ana.lamour@outlook.com>
2023-09-19 19:48:29 -0300

    modif

--- circle/circle.py
+++ circle/circle.py
@@ -1,31 +1,31 @@
 import math
 
-class Circle():
+
+class Circle:
+
     def __init__(self, radius):
         if radius <= 0:
-            raise ValueError ("El radio debe ser mayor a Cero")
+            raise ValueError("El radio debe ser mayor a Cero")
         self.radius = radius
 
     def get_radius(self):
         return self.radius
     
-    def set_radius (self, radius):
+    def set_radius(self, radius):
         if radius <= 0:
-            raise ValueError ("El radio debe ser mayor a cero")
+            raise ValueError("El radio debe ser mayor a cero")
         self.radius = radius
 
     def get_area(self):
-        return (math.pi * self.radius ** 2)
+        return math.pi * self.radius ** 2
     
     def get_perimeter(self):
-        return (2 * math.pi * self.radius)
+        return 2 * math.pi * self.radius
     
-    def __mul__(self, n):
-        if n <= 0:
+    def __mul__(self, radius):
+        if radius <= 0:
             raise ValueError("El mùltiplo debe ser mayor a cero")
-        return Circle(self.radius * n)
+        return Circle(self.radius * radius)
     
     def __str__(self):
         return f"Circle with radius {self.radius}"
-
-    pass
