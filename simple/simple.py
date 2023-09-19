0880b93b02e1574e6ba6d7ec4a7c15e419c41bf1
analamour <ana.lamour@outlook.com>
2023-09-19 20:39:26 -0300

    simple_list

--- simple/simple.py
+++ simple/simple.py
@@ -1,9 +1,18 @@
 import random
 
-
 def simple_list():
-    pass
+    persons = []
+    
+    for i in range(10):
+        person = {
+            "id": i,
+            "age": random.randint(1, 100)
+        }
+        persons.append(person)   
+    return persons
 
+def sort_list(dicts):
+    return sorted(dicts, key=lambda x: x['age'])
 
 def sort_list():
     pass
